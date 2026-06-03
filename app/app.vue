2223c88b059beae46045d49bc4949ea161addd97
Nilima Kumar <kumarnilima1512@gmail.com>
2026-06-03 11:24:26 +0530

    remove bg test

--- app/app.vue
+++ app/app.vue
@@ -1,15 +1,15 @@
 <template>
-  <div>
+  
     <Appbackground />
 
     <NuxtPage />
 
-    <div class="fixed bottom-4 right-4 z-50 pointer-events-none">
+    <!-- <div class="fixed bottom-4 right-4 z-50 pointer-events-none">
       <img
         src="/images/nil-logobg.png"
         alt="Logo"
         class="w-20 md:w-24 opacity-70"
       />
-    </div>
-  </div>
+    </div> -->
+  
 </template>
